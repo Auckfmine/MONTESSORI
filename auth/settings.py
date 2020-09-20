271866472a5dd37061117e2f06836a1ac535cc5a
AuckFmine <mouhamedaminerouatbi@gmail.com>
2020-09-21 00:28:31 +0100

    azg

--- auth/settings.py
+++ auth/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "dp-0nzfpkr)cs14^$t(vzin()1_^70=)#^-1t0!*%2pgdap4)n"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['10.0.2.2', 'localhost','127.0.0.1']
 
