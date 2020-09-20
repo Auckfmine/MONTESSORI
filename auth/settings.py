ea412676465560860c2d017a89751d7da03cc2aa
AuckFmine <mouhamedaminerouatbi@gmail.com>
2020-09-20 22:36:15 +0100

    second commit

--- auth/settings.py
+++ auth/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dp-0nzfpkr)cs14^$t(vzin()1_^70=)#^-1t0!*%2pgdap4)n'
+SECRET_KEY = os.environ("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
